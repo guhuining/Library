3690f9edcbc5e3a12d13b4e3ba5da532bcb17a79
huining <305579164@qq.com>
2020-12-13 12:37:45 +0800

    [fix]:修改获取配置方法的位置

--- config/config.go
+++ config/config.go
@@ -1,5 +1,10 @@
 package config
 
+import (
+	"gopkg.in/yaml.v3"
+	"io/ioutil"
+)
+
 type Server struct {
 	Host string `yaml:"host"`
 	Port string `yaml:"port"`
@@ -11,4 +16,29 @@ type Sql struct {
 	Host     string `yaml:"host"`
 	Port     string `yaml:"port"`
 	DBName   string `yaml:"db_name"`
+}
+
+// 返回服务器相关配置
+func (server *Server) ServerConfig() {
+	serverConfig, err := ioutil.ReadFile("config.yml") //读取配置文件
+	if err != nil {
+		panic(err)
+	}
+	err = yaml.Unmarshal(serverConfig, &server)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// 返回数据库相关配置
+func (sql *Sql) SqlConfig() {
+
+	sqlConfig, err := ioutil.ReadFile("config.yml") //读取配置文件
+	if err != nil {
+		panic(err)
+	}
+	err = yaml.Unmarshal(sqlConfig, &sql)
+	if err != nil {
+		panic(err)
+	}
 }
