44f804563aef939627b3ca2244b625e096daffe6
huining <305579164@qq.com>
2020-12-13 12:58:37 +0800

    [fix]:修改无法获取到配置的bug

--- config/config.go
+++ config/config.go
@@ -5,12 +5,17 @@ import (
 	"io/ioutil"
 )
 
+type Config struct {
+	Server   Server   `yaml:"server"`
+	Database Database `yaml:"database"`
+}
+
 type Server struct {
 	Host string `yaml:"host"`
 	Port string `yaml:"port"`
 }
 
-type Sql struct {
+type Database struct {
 	UserName string `yaml:"user_name"`
 	Password string `yaml:"password"`
 	Host     string `yaml:"host"`
@@ -19,25 +24,27 @@ type Sql struct {
 }
 
 // 返回服务器相关配置
-func (server *Server) Get() {
-	serverConfig, err := ioutil.ReadFile("config.yml") //读取配置文件
+func (config *Config) GetServer() Server {
+	serverConfig, err := ioutil.ReadFile("config/config.yml") //读取配置文件
 	if err != nil {
 		panic(err)
 	}
-	err = yaml.Unmarshal(serverConfig, &server)
+	err = yaml.Unmarshal(serverConfig, &config)
 	if err != nil {
 		panic(err)
 	}
+	return config.Server
 }
 
 // 返回数据库相关配置
-func (sql *Sql) Get() {
-	sqlConfig, err := ioutil.ReadFile("config.yml") //读取配置文件
+func (config *Config) GetDatabase() Database {
+	sqlConfig, err := ioutil.ReadFile("config/config.yml") //读取配置文件
 	if err != nil {
 		panic(err)
 	}
-	err = yaml.Unmarshal(sqlConfig, &sql)
+	err = yaml.Unmarshal(sqlConfig, &config)
 	if err != nil {
 		panic(err)
 	}
+	return config.Database
 }
