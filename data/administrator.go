b9623f5d79480db100334da37356b34e75584ca8
huining <305579164@qq.com>
2020-12-13 22:17:56 +0800

    [feat]:按用户名查找系统管理员信息

--- data/administrator.go
+++ data/administrator.go
@@ -10,3 +10,13 @@ func (administrator *Administrator) Create() (err error) {
 	_, err = Db.Query(statement, administrator.UserName, administrator.Password)
 	return
 }
+
+// @title		Administrator.GetPassword
+// @description	根据账号获取管理员密码
+// @param		Administrator.UserName	string	"用户名"
+// @return		err						error	"错误信息"
+func (administrator *Administrator) RetrieveByUserName() (err error) {
+	statement := "SELECT administratorID, password FROM Administrator WHERE username=?"
+	err = Db.QueryRow(statement, administrator.UserName).Scan(&administrator.AdministratorID, &administrator.Password)
+	return
+}
