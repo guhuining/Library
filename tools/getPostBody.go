35e9f2945a13016f282689826ee9384e429b7f80
huining <305579164@qq.com>
2020-12-13 12:01:51 +0800

    [feat]:工具函数，获取请求内容

---
+++ tools/getPostBody.go
@@ -0,0 +1,24 @@
+package tools
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// @title getPostBody
+// @description 从请求body中读取数据并转换为map
+// @return	postData	map[string]interface{}	"请求中的post信息"
+// @return	err 		error					"错误信息"
+func getPostBody(w http.ResponseWriter, r *http.Request) (postData map[string]interface{}, err error) {
+	// 获取请求body
+	bodyLen := r.ContentLength
+	body := make([]byte, bodyLen)
+	_, err = r.Body.Read(body)
+	// 将请求body中json格式的数据读取到map中
+	err = json.Unmarshal(body, &postData)
+	if err != nil {
+		w.Write(apiReturn(-1, "json form error", nil))
+		return
+	}
+	return
+}
