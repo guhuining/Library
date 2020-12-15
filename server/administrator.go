71da2b400bf529530a3e57aaafd26b266fd81fe3
GuHuining <305579164@qq.com>
2020-12-15 11:07:21 +0800

    [feat]: 更改部分接口所在位置

--- server/administrator.go
+++ server/administrator.go
@@ -121,3 +121,128 @@ func DeleteLibrarian(w http.ResponseWriter, r *http.Request) {
 		w.Write(tools.ApiReturn(0, "删除成功", nil))
 	}
 }
+
+// @title	AddPublication
+// @description	添加出版物
+// @param	w	http.ResponseWriter
+// @param	r	*http.Request
+func AddPublication(w http.ResponseWriter, r *http.Request) {
+	postData, err := tools.GetPostBody(w, r)
+	if err != nil {
+		return
+	}
+	// 鉴权
+	ok, err := authorizeAdministrator(r)
+	if err != nil {
+		w.Write(tools.ApiReturn(1, "服务器错误", nil))
+		return
+	} else if !ok {
+		w.Write(tools.ApiReturn(1, "权限不足", nil))
+		return
+	}
+	publication := &data.Publication{
+		Name:            postData["Name"].(string),
+		ISBN:            postData["ISBN"].(string),
+		Price:           int64(postData["Price"].(float64)),
+		Total:           int64(postData["Total"].(float64)),
+		Inventory:       int64(postData["Total"].(float64)),
+		PublicationType: data.PublicationType{PublicationType: postData["PublicationType"].(string)},
+		Author:          postData["Author"].(string),
+	}
+	err = publication.Insert()
+	if err != nil {
+		w.Write(tools.ApiReturn(1, "服务器错误", nil))
+	} else {
+		w.Write(tools.ApiReturn(0, "添加成功", nil))
+	}
+}
+
+// @title	DeletePublication
+// @description	删除出版物
+// @param	w	http.ResponseWriter
+// @param	r	*http.Request
+func DeletePublication(w http.ResponseWriter, r *http.Request) {
+	postData, err := tools.GetPostBody(w, r)
+	if err != nil {
+		return
+	}
+	// 鉴权
+	ok, err := authorizeAdministrator(r)
+	if err != nil {
+		w.Write(tools.ApiReturn(1, "服务器错误", nil))
+		return
+	} else if !ok {
+		w.Write(tools.ApiReturn(1, "权限不足", nil))
+		return
+	}
+	publication := &data.Publication{
+		PublicationID: int64(postData["PublicationID"].(float64)),
+	}
+	err = publication.Delete()
+	if err != nil {
+		w.Write(tools.ApiReturn(1, "服务器错误", nil))
+	} else {
+		w.Write(tools.ApiReturn(0, "删除成功", nil))
+	}
+}
+
+// @title	AddPublicationType
+// @description	添加出版物类型
+// @param	w	http.ResponseWriter
+// @param	r	*http.Request
+func AddPublicationType(w http.ResponseWriter, r *http.Request) {
+	postData, err := tools.GetPostBody(w, r)
+	if err != nil {
+		return
+	}
+	// 鉴权
+	ok, err := authorizeAdministrator(r)
+	if err != nil {
+		w.Write(tools.ApiReturn(1, "服务器错误", nil))
+		return
+	} else if !ok {
+		w.Write(tools.ApiReturn(1, "权限不足", nil))
+		return
+	}
+	publicationType := &data.PublicationType{
+		PublicationType: postData["PublicationType"].(string),
+		Fine:            int64(postData["Fine"].(float64)),
+	}
+	err = publicationType.Insert()
+	if err != nil && strings.Index(err.Error(), "Duplicate") != -1 {
+		w.Write(tools.ApiReturn(1, "已有该出版物类型", nil))
+	} else if err != nil {
+		w.Write(tools.ApiReturn(1, "服务器错误", nil))
+	} else {
+		w.Write(tools.ApiReturn(0, "添加成功", nil))
+	}
+}
+
+// @title	DeletePublicationType
+// @description	删除出版物类型
+// @param	w	http.ResponseWriter
+// @param	r	*http.Request
+func DeletePublicationType(w http.ResponseWriter, r *http.Request) {
+	postData, err := tools.GetPostBody(w, r)
+	if err != nil {
+		return
+	}
+	// 鉴权
+	ok, err := authorizeAdministrator(r)
+	if err != nil {
+		w.Write(tools.ApiReturn(1, "服务器错误", nil))
+		return
+	} else if !ok {
+		w.Write(tools.ApiReturn(1, "权限不足", nil))
+		return
+	}
+	publicationType := &data.PublicationType{
+		PublicationType: postData["PublicationType"].(string),
+	}
+	err = publicationType.Delete()
+	if err != nil {
+		w.Write(tools.ApiReturn(1, "服务器错误", nil))
+	} else {
+		w.Write(tools.ApiReturn(0, "删除成功", nil))
+	}
+}
