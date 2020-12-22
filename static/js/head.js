3f1a66182baa425c3f3b976da5cd298c06572758
huining <305579164@qq.com>
2020-12-22 22:18:43 +0800

    [feat]: 在导航条中显示当前登录状态并执行相应操作

---
+++ static/js/head.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+    // 判断是否登录
+    $.ajax({
+        type: "POST",
+        url: "/api/is_login",
+        contentType: "application/json; charset=utf-8",
+        data: null,
+        dataType: "json",
+        success: function (data) {
+            if(data["code"] === 0) {
+                let h = `
+                <a id="Logout" href="javascript:void(0)" onclick="Logout()">退出</a>
+                `
+                $("#IsLogin").html(h)
+            } else {
+                let h = `
+                <a id ="Login" href="/login.html">登录</a>
+                `
+                $("#IsLogin").html(h)
+            }
+        },
+        error: function (message) {
+            alert("error");
+        }
+    });
+
+});
+
+//退出登录
+function Logout(){
+    $.ajax({
+        type: "POST",
+        url: "/api/logout",
+        contentType: "application/json; charset=utf-8",
+        data: null,
+        dataType: "json",
+        success: function (data) {
+            if(data["code"] === 0) {
+                window.location.replace("/login.html")
+            } else {
+                alert(data["msg"])
+            }
+        },
+        error: function (message) {
+            alert("error")
+        }
+    })
+}
