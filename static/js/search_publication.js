954ebfa45eb21fa3c16c3ade1010acc6b192d28b
GuHuining <305579164@qq.com>
2020-12-23 14:42:03 +0800

    [feat]:处理未搜索到书籍的情况

--- static/js/search_publication.js
+++ static/js/search_publication.js
@@ -17,38 +17,45 @@ $(document).ready(function (){
             data: JSON.stringify(Data),
             dataType: "json",
             success: function (data) {
-                let h =
-                    `
-                <table class="table table-hover">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>书名</th>
-                            <th>作者</th>
-                            <th>库存/总量</th>
-                            <th>种类</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-               `;
-                // 填充表格
-                $.each(data["data"]["Publications"], function (index, element){
+                let h = `
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>书名</th>
+                                    <th>作者</th>
+                                    <th>库存/总量</th>
+                                    <th>种类</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                       `;
+                if (data["data"]["Publications"] == null) {
                     h += `
-                    <tr>
-                        <input type="hidden" value="` + element["publication_id"] + `">
-                        <td>` + (index + 1) + `</td>
-                        <td>` + element["name"] + `</td>
-                        <td>` + element["author"] + `</td>
-                        <td>` + element["inventory"] + `/` + element["total"] + `</td>
-                        <td>` + element["publication_type"]["publication_type"] + `</td>
-                        <td><a href="javascript:void(0)" onclick="order(this)">预定</a></td>
-                    </tr>
-                    `
-                })
-                h += `
-                    </tbody>
-                </table>
-                `
+                            </tbody>
+                        </table>
+                        `;
+                    h += `<h3>未查找到相关书籍</h3>`
+                } else {
+                    // 填充表格
+                    $.each(data["data"]["Publications"], function (index, element){
+                        h += `
+                            <tr>
+                                <input type="hidden" value="` + element["publication_id"] + `">
+                                <td>` + (index + 1) + `</td>
+                                <td>` + element["name"] + `</td>
+                                <td>` + element["author"] + `</td>
+                                <td>` + element["inventory"] + `/` + element["total"] + `</td>
+                                <td>` + element["publication_type"]["publication_type"] + `</td>
+                                <td><a href="javascript:void(0)" onclick="order(this)">预定</a></td>
+                            </tr>
+                            `
+                            });
+                    h += `
+                            </tbody>
+                        </table>
+                        `;
+                }
                 $("#PublicationList").html(h);
             },
             error: function (message) {
