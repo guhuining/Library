c127810ea64319f50ea109a47e9dd0c9a9d941cf
huining <305579164@qq.com>
2020-12-24 16:16:30 +0800

    [feat]: 当未登录时跳转到登录页面

---
+++ static/js/administrator/jump.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    // 判断是否登录
+    $.ajax({
+        type: "POST",
+        url: "/api/is_login",
+        contentType: "application/json; charset=utf-8",
+        data: null,
+        dataType: "json",
+        success: function (data) {
+            if(data["code"] === -1) {
+                window.location.replace("/administrator/login.html");
+            }
+        },
+        error: function (message) {
+            alert("error");
+        }
+    });
+
+});
