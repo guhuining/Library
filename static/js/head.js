55010352586d75f690a19730279e7aa43fc34e2f
huining <305579164@qq.com>
2020-12-26 15:37:15 +0800

    [fix]:修改样式

--- static/js/head.js
+++ static/js/head.js
@@ -9,16 +9,7 @@ $(document).ready(function(){
         success: function (data) {
             if(data["code"] === 0) {
                 let h = `
-                <div class="dropdown">
-                    <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                        用户
-                        <span class="caret"></span>
-                    </button>
-                    <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-                        <li><a href="/user_info.html">个人中心</a></li>
-                        <li><a id="Logout" href="javascript:void(0)" onclick="Logout()">退出</a></li>
-                    </ul>
-                </div>
+                    <li><a id="Logout" href="javascript:void(0)" onclick="Logout()">退出</a></li>
                 `
                 $("#IsLogin").html(h)
             } else {
