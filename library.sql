9d4be21b4cfcfcb1b6101119359049c94d90d173
huining <305579164@qq.com>
2020-12-26 14:27:33 +0800

    [fix]:修改数据库

--- library.sql
+++ library.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.29, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.31, for Win64 (x86_64)
 --
--- Host: localhost    Database: library
+-- Host: 127.0.0.1    Database: library
 -- ------------------------------------------------------
--- Server version	5.7.29
+-- Server version	5.7.31-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -32,6 +32,16 @@ CREATE TABLE `administrator` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `administrator`
+--
+
+LOCK TABLES `administrator` WRITE;
+/*!40000 ALTER TABLE `administrator` DISABLE KEYS */;
+INSERT INTO `administrator` VALUES (1,'huining','gu20000927'),(2,'Test','asdfasdf');
+/*!40000 ALTER TABLE `administrator` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `borrower`
 --
 
@@ -47,8 +57,18 @@ CREATE TABLE `borrower` (
   UNIQUE KEY `Borrower_username_uindex` (`username`),
   UNIQUE KEY `Borrower_cardNO_uindex` (`cardNO`),
   CONSTRAINT `Borrower_card_cardNO_fk` FOREIGN KEY (`cardNO`) REFERENCES `card` (`cardNO`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COMMENT='借阅者';
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COMMENT='借阅者';
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `borrower`
+--
+
+LOCK TABLES `borrower` WRITE;
+/*!40000 ALTER TABLE `borrower` DISABLE KEYS */;
+INSERT INTO `borrower` VALUES (1,'284133','testuser','testuser'),(3,'284134','huining','123123'),(19,NULL,'huining12138','123123'),(21,NULL,'testtest','123123');
+/*!40000 ALTER TABLE `borrower` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `borrowertype`
@@ -64,6 +84,16 @@ CREATE TABLE `borrowertype` (
   PRIMARY KEY (`borrowerType`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `borrowertype`
+--
+
+LOCK TABLES `borrowertype` WRITE;
+/*!40000 ALTER TABLE `borrowertype` DISABLE KEYS */;
+INSERT INTO `borrowertype` VALUES ('本科生',14,5),('研究生',21,7);
+/*!40000 ALTER TABLE `borrowertype` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `borrowitem`
@@ -84,8 +114,18 @@ CREATE TABLE `borrowitem` (
   KEY `BorrowItem_publication_publicationID_fk` (`publicationID`),
   CONSTRAINT `BorrowItem_card_cardNO_fk` FOREIGN KEY (`cardNO`) REFERENCES `card` (`cardNO`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `BorrowItem_publication_publicationID_fk` FOREIGN KEY (`publicationID`) REFERENCES `publication` (`publicationID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `borrowitem`
+--
+
+LOCK TABLES `borrowitem` WRITE;
+/*!40000 ALTER TABLE `borrowitem` DISABLE KEYS */;
+INSERT INTO `borrowitem` VALUES (20,'284133',1,'2020-12-16 21:35:40',NULL,0),(21,'284133',1,'2020-12-16 21:35:41',NULL,0),(27,'284133',1,'2020-12-16 21:58:37',NULL,0),(28,'284133',1,'2020-12-17 16:13:48',NULL,0),(29,'284133',1,'2020-12-23 22:46:52',NULL,0);
+/*!40000 ALTER TABLE `borrowitem` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `card`
@@ -107,6 +147,16 @@ CREATE TABLE `card` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `card`
+--
+
+LOCK TABLES `card` WRITE;
+/*!40000 ALTER TABLE `card` DISABLE KEYS */;
+INSERT INTO `card` VALUES ('284133','张三','软件工程','本科生',5),('284134','李四','土木工程','研究生',0);
+/*!40000 ALTER TABLE `card` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `librarian`
 --
 
@@ -119,8 +169,18 @@ CREATE TABLE `librarian` (
   `password` varchar(20) NOT NULL,
   PRIMARY KEY (`librarianID`),
   UNIQUE KEY `Librarian_username_uindex` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COMMENT='图书管理员';
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COMMENT='图书管理员';
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `librarian`
+--
+
+LOCK TABLES `librarian` WRITE;
+/*!40000 ALTER TABLE `librarian` DISABLE KEYS */;
+INSERT INTO `librarian` VALUES (1,'huining','123456');
+/*!40000 ALTER TABLE `librarian` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `lostitem`
@@ -143,6 +203,15 @@ CREATE TABLE `lostitem` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `lostitem`
+--
+
+LOCK TABLES `lostitem` WRITE;
+/*!40000 ALTER TABLE `lostitem` DISABLE KEYS */;
+/*!40000 ALTER TABLE `lostitem` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `orderitem`
 --
 
@@ -160,28 +229,18 @@ CREATE TABLE `orderitem` (
   KEY `orderitem_publication_publicationID_fk` (`publicationID`),
   CONSTRAINT `orderitem_card_cardNO_fk` FOREIGN KEY (`cardNO`) REFERENCES `card` (`cardNO`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `orderitem_publication_publicationID_fk` FOREIGN KEY (`publicationID`) REFERENCES `publication` (`publicationID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `overtimeitem`
+-- Dumping data for table `orderitem`
 --
 
-DROP TABLE IF EXISTS `overtimeitem`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `overtimeitem` (
-  `overTimeItemID` int(11) NOT NULL AUTO_INCREMENT,
-  `borrowItemID` int(11) NOT NULL,
-  `cardNO` varchar(10) NOT NULL,
-  `dueDate` datetime NOT NULL,
-  PRIMARY KEY (`overTimeItemID`),
-  KEY `OverTimeItem_borrowitem_borrowItemID_fk` (`borrowItemID`),
-  KEY `OverTimeItem_card_cardNO_fk` (`cardNO`),
-  CONSTRAINT `OverTimeItem_borrowitem_borrowItemID_fk` FOREIGN KEY (`borrowItemID`) REFERENCES `borrowitem` (`borrowItemID`),
-  CONSTRAINT `OverTimeItem_card_cardNO_fk` FOREIGN KEY (`cardNO`) REFERENCES `card` (`cardNO`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `orderitem` WRITE;
+/*!40000 ALTER TABLE `orderitem` DISABLE KEYS */;
+INSERT INTO `orderitem` VALUES (1,1,'284133','2020-12-20 22:42:58',0),(4,3,'284134','2020-12-24 17:03:37',0);
+/*!40000 ALTER TABLE `orderitem` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `publication`
@@ -202,8 +261,18 @@ CREATE TABLE `publication` (
   PRIMARY KEY (`publicationID`),
   KEY `publication_publicationtype_publicationType_fk` (`publicationType`),
   CONSTRAINT `publication_publicationtype_publicationType_fk` FOREIGN KEY (`publicationType`) REFERENCES `publicationtype` (`publicationType`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `publication`
+--
+
+LOCK TABLES `publication` WRITE;
+/*!40000 ALTER TABLE `publication` DISABLE KEYS */;
+INSERT INTO `publication` VALUES (1,'C Primer Plus','978-7-115-13022-8',35,20,15,'图书','Stephen Prata'),(3,'三体','9877548345467',44,14,14,'图书','刘慈欣');
+/*!40000 ALTER TABLE `publication` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `publicationtype`
@@ -218,6 +287,16 @@ CREATE TABLE `publicationtype` (
   PRIMARY KEY (`publicationType`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `publicationtype`
+--
+
+LOCK TABLES `publicationtype` WRITE;
+/*!40000 ALTER TABLE `publicationtype` DISABLE KEYS */;
+INSERT INTO `publicationtype` VALUES ('图书',15),('杂志',10);
+/*!40000 ALTER TABLE `publicationtype` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -228,4 +307,4 @@ CREATE TABLE `publicationtype` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-12-15 16:22:03
+-- Dump completed on 2020-12-26 14:27:02
